f8a99b5693718accf2b551c1eca4c56cb4a9167a
marcantondahmen <marcantondahmen@users.noreply.github.com>
2026-02-01 15:55:59 +0000

    build version 2.0.0-beta.15

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'automad/automad',
-        'pretty_version' => '2.0.0-beta.14',
-        'version' => '2.0.0.0-beta14',
-        'reference' => '493a2bd1cc9e236cd902c141aab3e3fb0be7ee21',
+        'pretty_version' => '2.0.0-beta.15',
+        'version' => '2.0.0.0-beta15',
+        'reference' => '25743a400d0a820fd70bc8d38cd7e86b6939ae71',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automad/automad' => array(
-            'pretty_version' => '2.0.0-beta.14',
-            'version' => '2.0.0.0-beta14',
-            'reference' => '493a2bd1cc9e236cd902c141aab3e3fb0be7ee21',
+            'pretty_version' => '2.0.0-beta.15',
+            'version' => '2.0.0.0-beta15',
+            'reference' => '25743a400d0a820fd70bc8d38cd7e86b6939ae71',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
